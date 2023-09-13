66bb0d10370623be2bef389de420c74ee19f23a9
Adiba3924 <adiba15-3924@diu.edu.bd>
2023-09-13 16:03:39 +0600

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#change', {
+    strings: ["Undergraduate Student", "Graphics Designer", "UI/UX Designer", "Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
